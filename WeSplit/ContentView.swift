387d1456d8392ef4aeca068bc02a1348abad27f7
Luis Gamarra Jimenez <luis.a.gamarra.jimenez@disney.com>
2021-06-11 17:32:30 -0600

    Added state to an app using the state property wrapper

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tapCount: Int = 0
+    
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    Text("Hello, world!")
-                }
+        
+        VStack {
+            Text("Tap Count: \(tapCount)")
+            
+            Button("Increase Count") {
+                self.tapCount += 1
             }
-            .navigationBarTitle("SwiftUI")
         }
     }
 }
