28295f65a61645909fc469139c4d47c44344ef55
Luis Gamarra Jimenez <luis.a.gamarra.jimenez@disney.com>
2021-06-14 17:07:56 -0600

    Added finished project

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,19 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-
-    let friends: [String] = ["Daniel", "Diego", "Maggie"]
-    @State private var selectedFriend: Int = 0
     
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
     
     var body: some View {
-        VStack {
-            Picker("Select your favorite friends", selection: $selectedFriend) {
-                ForEach(0 ..< friends.count) {
-                    Text(self.friends[$0])
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Check Amount: ", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Section(header: Text("How much tip do you want to leave?")) {
+                        Picker("Tip Percentage", selection: $tipPercentage) {
+                            ForEach(0 ..< tipPercentages.count) {
+                                Text("\(self.tipPercentages[$0])%")
+                            }
+                        }.pickerStyle(SegmentedPickerStyle())
+                    }
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
             }
-            Text("You chose: \(friends[selectedFriend])")
+            .navigationTitle("WeSplit")
         }
     }
 }
